d722aa92f960929049094a4276c8a0254dc8b6b8
mac <mac@CY-MacBook-Pro.local>
2023-06-27 22:29:48 +0900

    first commit

---
+++ src/main/java/com/example/productorderservie/ProductOrderServieApplication.java
@@ -0,0 +1,13 @@
+package com.example.productorderservie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderServieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrderServieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productorderservie/ProductOrderServieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productorderservie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderServieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
